d00ff60b28482759f1a4cff973896a3efa073ab6
Lucas Sartori <faltou.criatividade0@gmail.com>
2017-11-15 12:26:04 -0200

    ajuste nas rotas de consumo da api

--- src/app/shared/routes-api/routes-client.util.ts
+++ src/app/shared/routes-api/routes-client.util.ts
@@ -2,14 +2,14 @@
  * Created by sartori on 16/03/17.
  */
 export class RoutesClientUtil {
-  public static GUNS = 'armas/';
-  public static CLIENTS: string = 'usuarios/';
-  public static SEX: string = 'sexos/';
-  public static LVL_ACCESS: string = 'usuarios/niveis/acessos/';
-  public static LICENCES: string = 'licencas/';
-  public static LICENCES_TYPES: string = 'licencas/categorias/';
-  public static EXAMS: string = 'exames/';
-  public static EXAMS_TYPES: string = 'exames/categorias/';
+  public static GUNS= 'armas/';
+  public static CLIENTS= 'usuarios/';
+  public static SEX= 'sexos/';
+  public static LVL_ACCESS= 'usuarios-niveis-acessos/';
+  public static LICENCES= 'licencas/';
+  public static LICENCES_TYPES= 'licencas-categorias/';
+  public static EXAMS= 'exames/';
+  public static EXAMS_TYPES= 'exames-categorias/';
 
   /**   /find/property/{property}/value/{value}
    *    /property/{property}/value/{value}
