5ac4f36daebe322d5802121e4bee3a300d58008a
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-02-21 21:44:08 -0300

    correcao de bug em client profile

--- src/app/client/client-profile/client-profile.component.ts
+++ src/app/client/client-profile/client-profile.component.ts
@@ -3,7 +3,7 @@ import {User} from "../../shared/model/user";
 import {UserService} from "../../shared/services/user.service";
 import {GenderService} from "../../shared/services/gender.service";
 import {Gender} from "../../shared/model/gender";
-
+import {HttpErrorService} from "../../shared/services/http-error.service";
 
 @Component({
   selector: 'app-client-profile',
@@ -17,13 +17,14 @@ export class ClientProfileComponent implements OnInit {
 
   constructor(private userService: UserService,
               private genderService: GenderService,
-              private httpErrorService) {
+              private httpErrorService: HttpErrorService) {
   }
 
   ngOnInit() {
     this.userService.getById(sessionStorage.getItem("currentUserID")).subscribe(res => {
       this.model = res;
-      this.genderService.getById(res.gender.id).subscribe(res => this.gender = res, error2 => this.httpErrorService.verifyErrors(error2));
+      this.genderService.getById(res.gender.id)
+        .subscribe(res => this.gender = res, error2 => this.httpErrorService.verifyErrors(error2));
     }, error2 => this.httpErrorService.verifyErrors(error2));
   }
 }
--- src/app/shared/services/http-error.service.ts
+++ src/app/shared/services/http-error.service.ts
@@ -17,14 +17,20 @@ export class HttpErrorService {
                message401?: string,
                message500?: string): void {
 
+    console.log(JSON.stringify(error));
+
+    let msg404 = message404 != null ? message404 : "Não foi possivel encontrar com os dados fornecidos.\n\t Por Favor verifique os dados informados";
+    let msg401 = message401 != null ? message401 : "Sinto muito sua sessão expirou.\n\t por favor faça novamente o Login";
+    let msg500 = message500 != null ? message500 : "Erro no servidor, por favor tente mais tarde";
+
     if (error.status == ErrorsHttp.ERRO_404)
-      this.openDialog("Alerta", message404 != null ? message404 : "Não foi possivel encontrar com os dados fornecidos.\n\t Por Favor verifique os dados informados", "OK");
+      this.openDialog("Alerta", msg404, "OK");
     else if (error.status == ErrorsHttp.ERRO_401) {
-      this.openDialog("Atenção", message401 != null ? message401 : "Sinto muito sua sessão expirou.\n\t por favor faça novamente o Login", "OK");
+      this.openDialog("Atenção", msg401, "OK");
       this.authService.logout();
     }
     else if (error.status == ErrorsHttp.ERRO_500)
-      this.openDialog("Erro", message500 != null ? message500 : "Erro no servidor, por favor tente mais tarde", "OK");
+      this.openDialog("Erro", msg500, "OK");
     else
       this.openDialog("Erro", defaultMessage != null ? defaultMessage : JSON.stringify(error), "OK");
   }
