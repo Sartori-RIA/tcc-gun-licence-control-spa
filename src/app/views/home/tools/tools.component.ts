75f6ee692ba0ba009866f98a0726ba23977db128
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-03-20 23:02:32 -0300

    wowjs adicionado

--- src/app/views/home/tools/tools.component.ts
+++ src/app/views/home/tools/tools.component.ts
@@ -1,14 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {WOW} from 'wowjs/dist/wow.min';
 
 @Component({
   selector: 'app-tools',
   templateUrl: './tools.component.html',
   styleUrls: ['./tools.component.scss']
 })
-export class ToolsComponent implements OnInit {
+export class ToolsComponent implements OnInit, AfterViewInit {
   @Input('backgroundGray') public backgroundGray;
 
   constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    new WOW().init();
   }
 
   ngOnInit() {
