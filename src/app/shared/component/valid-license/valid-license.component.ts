ba02a07bd96882df34ce54f6406cf4fa7ffbf706
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-04-12 23:33:37 -0300

    criado template para impressao

--- src/app/shared/component/valid-license/valid-license.component.ts
+++ src/app/shared/component/valid-license/valid-license.component.ts
@@ -31,7 +31,68 @@ export class ValidLicenseComponent implements OnInit {
     return this.license.status;
   }
 
-  licenseSerialUrl():string{
+  licenseSerialUrl(): string {
     return `${RoutesClientUtil.VALIDATOR}find/serial/${this.license.serial}`
+  }
+
+  printLicense() {
+    let licenceHeader =
+      `<head>
+          <meta charset="utf-8" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+      </head>`;
+    let licenseStyle =
+      `<style>
+           .max-license-width{
+            width: 500px;
+           }
+       </style>`;
+    let licenceBody = `<body>
+      <div class="row">
+          <div class="col s12 m7">
+             <div class="card max-license-width">
+                 <div class="card-title center-align"><strong>Portador: </strong>${this.formatCamelCase(this.license.user.name)}</div>
+                     <div class="card-content">
+                         <div><p><strong>Numero de serie: </strong>${this.license.serial}</p></div>
+                         <div><p><strong>Categoria: </strong>${this.license.category.description}</p> </div>
+                         <div><p><strong>Situação: </strong>${this.licenseSituation(this.license.status)}</p></div>
+                         <div><p><strong>Validade até: </strong>${this.licenseExpiration(this.license.shelfLife)}</p></div>
+                         <div><p><strong>CPF: </strong>${this.formatCPF(this.license.user.cpf)}</p></div>
+                         <div><p><strong>RG: </strong>${this.license.user.rg}</p></div>
+                         <div><p><strong>Rua: </strong>${this.license.address.street} - ${this.license.address.addressNumber}</p></div>
+                         <div><p><strong>Cidade: </strong>${this.license.address.city.description}</p></div>
+                         <div><p><strong>Estado: </strong>${this.license.address.city.state.description}</p></div>
+                         <div><p><strong>Pais: </strong>${this.license.address.city.state.country.description}</p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+      </body>`;
+
+    window.document.write(licenceHeader + licenseStyle + licenceBody);
+    window.print();
+  }
+
+  private formatCamelCase(value: string): string {
+    let values = value.split(' ');
+    let result = '';
+    for (let v of values)
+      result += this.capitalize(v) + ' ';
+    return result;
+  }
+
+  private capitalize(value: string) {
+    return value.substr(0, 1).toUpperCase() +
+      value.substr(1).toLowerCase();
+  }
+
+  private formatCPF(value: string): string {
+    return value.substring(0, 3).concat('.')
+      .concat(value.substring(3, 6))
+      .concat('.')
+      .concat(value.substring(6, 9))
+      .concat('-')
+      .concat(value.substring(9, 11))
   }
 }
